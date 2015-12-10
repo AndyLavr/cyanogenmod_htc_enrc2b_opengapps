d349ddcde943d15dffe331491c7591448390701a
Andrei Lavreniyuk <andy.lavr@gmail.com>
2015-12-10 13:12:11 +0200

    Update inc.buildtarget.sh script file (remove unused architectures)

--- scripts/inc.buildtarget.sh
+++ scripts/inc.buildtarget.sh
@@ -89,16 +89,12 @@ mms
 picotts"
 
 case "$ARCH" in
-  arm64|x86_64)  LIBFOLDER="lib64";;
   *)             LIBFOLDER="lib";;
 esac
 
 get_fallback_arch(){
   case "$1" in
     arm)    fallback_arch="all";;
-    arm64)  fallback_arch="arm";;
-    x86)    fallback_arch="arm";; #by using libhoudini
-    x86_64) fallback_arch="x86";; #e.g. chain: x86_64->x86->arm->all
     *)      fallback_arch="$1";; #return original arch if no fallback available
   esac
 }
@@ -194,15 +190,15 @@ get_package_info(){
                     else
                       packagelibs="libfilterpack_facedetect.so";
                     fi;;
-    faceunlock)     case "$ARCH" in #only arm based platforms
-                      arm*) packagetype="GApps"; packagename="com.android.facelock"; packagetarget="app/FaceLock";
-                            packagefiles="vendor/pittpatt/";
-                            if [ "$LIBFOLDER" = "lib64" ]; then #on 64 bit, we also need the 32 bit lib of librsdk.so
-                              packagelibs="libfacelock_jni.so libfrsdk.so+fallback";
-                            else
-                              packagelibs="libfacelock_jni.so libfrsdk.so";
-                            fi;;
-                    esac;;
+    faceunlock)     if [ "$ARCH" = "arm" ] || [ "$ARCH" = "arm64" ]; then #both arm and arm64
+                      packagetype="GApps"; packagename="com.android.facelock"; packagetarget="app/FaceLock";
+                      packagefiles="vendor/pittpatt/";
+                      if [ "$LIBFOLDER" = "lib64" ]; then #on 64 bit, we also need the 32 bit lib of librsdk.so
+                        packagelibs="libfacelock_jni.so libfrsdk.so+fallback";
+                      else
+                        packagelibs="libfacelock_jni.so libfrsdk.so";
+                      fi
+                    fi;;
     fitness)        packagetype="GApps"; packagename="com.google.android.apps.fitness"; packagetarget="app/FitnessPrebuilt";;
     gmail)          packagetype="GApps"; packagename="com.google.android.gm"; packagetarget="app/PrebuiltGmail";;
     googlenow)      packagetype="GApps"; packagename="com.google.android.launcher"; packagetarget="app/GoogleHome";;
