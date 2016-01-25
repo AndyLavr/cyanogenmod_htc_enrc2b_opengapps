7ea5c565eefb224c704c5890fe0876f9607cdf30
Andrei Lavreniyuk <andy.lavr@gmail.com>
2016-01-25 15:11:25 +0200

    Add new CM's Snap camera to stock cameras list

--- scripts/inc.installdata.sh
+++ scripts/inc.installdata.sh
@@ -222,6 +222,7 @@ priv-app/Camera'"$REMOVALSUFFIX"'
 priv-app/Camera2'"$REMOVALSUFFIX"'
 app/MotCamera'"$REMOVALSUFFIX"'
 priv-app/MotCamera'"$REMOVALSUFFIX"'
+app/Snap'"$REMOVALSUFFIX"'
 ";
 
 clockstock_list="
